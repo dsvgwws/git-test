14687c0bd1eab730d7643abf5a059f3f20a9d659
horizon <cui_went@163.com>
2021-09-12 12:35:59 +0800

    second commit

--- src/main/java/com/horizon/git/GitTest.java
+++ src/main/java/com/horizon/git/GitTest.java
@@ -3,5 +3,6 @@ package com.horizon.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git2");
     }
 }
